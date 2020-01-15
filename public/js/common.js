0abba76105371ef08778ec3f608455601c50ca0a
Aleksandr3012 <flaming541@gmail.com>
2020-01-15 13:48:10 +0200

    edits made

--- public/js/common.js
+++ public/js/common.js
@@ -126,7 +126,7 @@ function eventHandler() {
 	JSCCommon.inputMask(); // JSCCommon.CustomInputFile();
 	// добавляет подложку для pixel perfect
 
-	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/14.png);"></div>'); // /добавляет подложку для pixel perfect
+	$(".main-wrapper").after('<div class="screen" style="background-image: url(screen/7.png);"></div>'); // /добавляет подложку для pixel perfect
 	// /закрыть/открыть мобильное меню
 	// листалка по стр
 
